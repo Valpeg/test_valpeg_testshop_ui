7e7d45815145d2016f14493e69a54fbbeeeeb970
Валерий Пегушин <valpegushin@yandex.ru>
2026-08-26 17:34:49 +0500

    remove remote-debugging-port, let Selenium manage chromedriver

--- conftest.py
+++ conftest.py
@@ -13,8 +13,6 @@ def driver():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
-    options.add_argument('--disable-software-rasterizer')
-    options.add_argument('--remote-debugging-port=9222')
     chrome_driver = webdriver.Chrome(options=options)
     chrome_driver.set_page_load_timeout(30)
     chrome_driver.implicitly_wait(5)
