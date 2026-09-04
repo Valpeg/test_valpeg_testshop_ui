24dcfe9d4b7eea86cb882ce8fe3ddabc05a5c936
Valpeg <valpegushin@yandex.ru>
2026-09-04 19:58:10 +0500

    add allure screenshot and update requirements

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
+import allure
+from allure_commons.types import AttachmentType
 from pages.order_overview import OrderOverview
 from pages.products_desks import ProductsDesks
 from pages.main_page import MainPage
@@ -14,10 +16,16 @@ def driver():
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
     chrome_driver = webdriver.Chrome(options=options)
-    chrome_driver.set_page_load_timeout(30)
-    chrome_driver.implicitly_wait(5)
+    chrome_driver.set_page_load_timeout(60)
+    chrome_driver.implicitly_wait(10)
     chrome_driver.maximize_window()
     yield chrome_driver
+    allure.attach(
+        chrome_driver.get_screenshot_as_png(),
+        name="Screenshot",
+        attachment_type=AttachmentType.PNG
+    )
+    chrome_driver.quit()
 
 
 @pytest.fixture()
